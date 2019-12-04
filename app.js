b801320e78e4ffae5cd34645d7f48972a57e4f2d
Mohd Imran <imrancrazy200@gmail.com>
2019-12-04 21:27:33 +0530

    complete app.js

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const request = require('request');
 
-const apiKey = 'N5YtIhtLmCZzgha9wVknDfpOKNbEcC1DwOCytxHm';
+const apiKey = 'YOUR API KEY'; //check out README.md to get your NASA Open API Key
 const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
-const imagePath = path.join(__dirname, 'images');
 
 request(url, {json:true}, (err, res, body) => {
     if(err) return console.log(err);
@@ -13,8 +12,25 @@ request(url, {json:true}, (err, res, body) => {
 })
 
 function saveImage(url) {
-    request(url, (err, res, body) => {
+    let file;
+    request.head(url, (err, res, body) => {
         if(err) return console.log(err);
-        console.log(res);
+        if(res.headers['content-type'] === 'image/jpeg')
+        {
+             file = fs.createWriteStream('image.jpeg');
+        }
+        else if(res.headers['content-type'] === 'image/jpg') 
+        {
+             file = fs.createWriteStream('image.jpg');
+        }
+        else if(res.headers['content-type'] === 'image/png') 
+        {
+             file = fs.createWriteStream('image.png');
+        }
+        else
+        {
+            console.log('Wrong Content Type ! Its not a valid image !');
+        }
+        request(url).pipe(file);
     })
 }
